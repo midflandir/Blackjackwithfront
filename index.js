0903a3f9f0632aa86df7f2b2d42406a01dc639e6
midflandir <juancamilo372cp@live.com>
2022-07-14 15:57:56 -0500

    animations added confettti

--- index.js
+++ index.js
@@ -1,30 +1,56 @@
 let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J"];
+let suits = ["♦", "♣", "♥", "♠"];
 //suit: heart, diamond, ace and clover
 
 let selectedCards = []; //First and second card -randomly. And then add with a draw function
+let selectedCardssuits = [];
 let totalgamecounter = 0;
 let sum = 0;
 let answer = "";
 let Avalue = "10";
 
+
+function loadpage(){
+    document.getElementById("newgamebutton").style.display = "block";
+    document.getElementById("drawcardbutton").style.display = "none";
+    document.getElementById("ChangeAbutton").style.display = "none";
+    document.getElementById("Exitgamebutton").style.display = "none";
+}
+function exitgame(){
+    window.location.reload();
+}
+
+
 function startGame() {
   //TO DO: Add the first 2 cards randomly to the user
   //Math.random() - 0 - cards.length
-  selectedCards = [];
-  cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J"];
-  document.getElementById("gamestats").innerText = " ";
-  document.getElementById("gamesprize").innerText = " ";
+  let i_suit;
+  let suit;
+ // selectedCards = [];
+ // cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J"];
+ // document.getElementById("gamestats").innerText = " ";
+ // document.getElementById("gamesprize").innerText = " ";
 
+  document.getElementById("newgamebutton").style.display = "none";
   document.getElementById("drawcardbutton").style.display = "block";
   document.getElementById("ChangeAbutton").style.display = "block";
+  document.getElementById("Exitgamebutton").style.display = "block";
 
   const i_firstCard = Math.floor(Math.random() * cards.length);
   const firstCard = cards[i_firstCard];
   cards.splice(i_firstCard, 1);
 
+   i_suit = Math.floor(Math.random() * suits.length);
+   suit = suits[i_suit];
+   selectedCardssuits.push(suit);
+
   const i_secondCard = Math.floor(Math.random() * cards.length);
   const secondCard = cards[i_secondCard];
   cards.splice(i_secondCard, 1);
+
+  i_suit = Math.floor(Math.random() * suits.length);
+  suit = suits[i_suit];
+  selectedCardssuits.push(suit);
 
   selectedCards.push(firstCard);
   selectedCards.push(secondCard);
@@ -32,10 +58,20 @@ function startGame() {
   gamestatus();
 }
 
+function suitgenerator(){
+  const i_suit = Math.floor(Math.random() * 5);
+  const suit = cards[i_suit];
+
+return
+}
+
+
+
+
 function gamestatus() {
-  console.log("Your Cards: " + selectedCards);
+  console.log("Your Cards: " + selectedCards + selectedCardssuits);
   document.getElementById("yourcards").innerText =
-    "Your Cards: " + selectedCards;
+    "Your Cards: " + selectedCards + selectedCardssuits;
 
   let cardssum = sumCards(selectedCards);
   console.log("Sum: " + cardssum + " \n");
@@ -46,7 +82,7 @@ function gamestatus() {
     console.log(
       "   Blackjack!!! \n  -----  You win!!!! \n"
     );
-
+    addConfetti();
     document.getElementById("gamestats").innerText =
       "   Blackjack!!!  You win!!!!   ";
       let prize= prizecalculator();
@@ -131,6 +167,29 @@ function drawCard() {
   selectedCards.push(newCard);
   cards.splice(i_newCard, 1);
 
+  let i_suit = Math.floor(Math.random() * suits.length);
+  let suit = suits[i_suit];
+  selectedCardssuits.push(suit);
+
   console.log(cards);
   gamestatus();
 }
+
+
+
+
+
+
+function addConfetti() {
+    for(var i=0;i<100;i++){
+      var c = document.createElement('div')
+      c.id = 'confetti';
+      c.className = 'confetti'
+      c.style.left = Math.random()*100 + '%'
+      c.style.background = 'hsl('+Math.random()*360+'deg,100%,50%)'
+      c.style.animationDuration = (Math.random()*3) + 2 + 's'
+      c.style.animationDelay = Math.random()*7 + 's'
+      document.body.appendChild(c)
+    }
+  }
+
